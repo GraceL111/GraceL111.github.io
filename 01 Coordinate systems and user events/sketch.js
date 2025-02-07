e8f23c7404fab64035da1b791bef33e28410de6f
Li7003958 <7003958@spsd.sk.ca>
2025-02-07 11:18:01 -0600

    Demo one done

---
+++ 01 Coordinate systems and user events/sketch.js
@@ -0,0 +1,67 @@
+// Coordinate System and User Events
+// Grace Li
+// Feb 6th , 2025
+
+// Not run to completion, but interactive programs...
+
+
+function setup() {
+  // runs once, at the very beginning...
+  createCanvas(500, 500);
+}
+
+function draw() {
+  // draw LOOP, repeats over and over forever...
+  // - target of 60 frames per second
+  // A new image is drawn at the bottom of the loop
+  //Dont dump everyting in draw function
+  background(220);
+  firstCircle();
+  secCircle();
+  thirdCircle();
+  fourthCircle();
+  centerCircle();
+}
+
+
+function firstCircle(){
+  //draws two circles, one at a fixed location and one at the mouse location
+  fill(0,255,0); //Green fill
+  stroke(0, 0, 255);  //Blue outline
+  strokeWeight(3);    //Thickness of outline
+  circle(0,0,50);
+
+
+  // Secret calculated delay()
+  // Screen updates at end of loop
+}
+
+function secCircle(){
+  fill(0,255,0); 
+  stroke(0, 0, 255); 
+  strokeWeight(3);    
+  circle(0,height, 50);
+}
+
+function thirdCircle(){
+  fill(0,255,0); 
+  stroke(0, 0, 255); 
+  strokeWeight(3);    
+  circle(width, 0, 50);
+}
+
+function fourthCircle(){
+  fill(0,255,0); 
+  stroke(0, 0, 255); 
+  strokeWeight(3);    
+  circle(width, height, 50);
+}
+
+function centerCircle(){
+  let cenCircleW = width/2;
+  let cenCircleH = height/2;
+  fill(0,255,0); 
+  stroke(0, 0, 255); 
+  strokeWeight(3);    
+  circle(cenCirleW, cenCircleH, 50);
+}
