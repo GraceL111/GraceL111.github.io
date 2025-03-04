07bdfa841c11e26c05b262575e4bc47dbabec6c7
Li7003958 <7003958@spsd.sk.ca>
2025-03-04 12:17:13 -0600

    March 4 Perlin Noise

--- Perlin Noise/sketch.js
+++ Perlin Noise/sketch.js
@@ -5,10 +5,36 @@
 //
 
 
+let rectWidth = 10;
+let rectHeight;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+  generateTerrain();
+}
+
+
+function generateTerrain(){
+  //use a loop to generate and draw
+  // several rectangles side to side
+  // to look like some 2D Terrain
+
+  rectMode(CORNERS);
+  for(let x = 0; x < width; x += rectWidth){
+    //generate a random height
+    let noiseNum = noise(5) * frameCount;
+    rectHeight = 100 * noiseNum;
+    // Find the other corner of our rectangle
+    let x2 = x + rectWidth;
+    let y2 = height - rectHeight;
+
+    rect(x, height, x2, y2);
+  }
+
+
+  rectMode(CORNER);
 }
