c4d0c1bef4ebe38cea06cc53e5087a20a555170c
Li7003958 <7003958@spsd.sk.ca>
2025-04-17 11:26:33 -0600

    April 17 second commit

--- 20b CSS Centering and 3D shapes/sketch.js
+++ 20b CSS Centering and 3D shapes/sketch.js
@@ -4,32 +4,50 @@
 //
 // 
 
-let angle = 5;
-
 function setup() {
-  createCanvas(500, 500, WEBGL);
-  angleMode(DEGREES);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);  // webgl has 0,0 at center, 
-  //translate(90, 0, 400);//need to use translate() to shift center of object
-  rotateX(-20);
-  rotateY(frameCount);
-  angle = map(mouseX, 0, width, -120, 120);
-  for(let i = 0; i < 360; i += 45){
-    push();
-    rotateY(i);
-    drawBox(30);
-    pop();
-  }
+  background(220);
+  circleFractal(width/2, height*.8, height*.75);
+  //cantor(width*0.1, height*0.3, width*0.8, 9);
 }
 
-function drawBox(size){
-  if(size < 3){
-    rotateZ(angle);
-    translate(size*1.5, 0);
-    box(size);
-    drawBox(size*0.8);
+
+function circleFractal(x,y,d){
+  noFill();
+  if(d > 1){
+    circle(x,y,d);
+    //recursive calls
+    circleFractal(x-d/2, y, d/2); //recursive circle to left
+    circleFractal(x+d/2, y, d/2); //recursive circle to right
+    circleFractal(x, y-d/2, d/2); //recursive circle to top
   }
+  //implicit base case - don't recurse if diameter is small
 }
+
+function cantor(x,y,len,depth){
+  if(depth > 1){
+    line(x,y, x+len, y);
+    y += 20;
+
+    cantor(x,y,len/3,depth-1); //left third
+    cantor(x + len*2/3, y, len/3, depth-1);
+  }
+  //otherwise, BASE CASE unravel
+}
+
+
+
+
+
+// function reCircle(x,y,d){
+//   //recursively draw circles as long as 
+//   //diameter > 5
+//   circle(x,y,d);
+//   if (d >= 10){  //Recursive Call
+//     reCircle(x,y,d*0.9)
+//   }
+//   //Implicit base case (if d <10)
+// }
--- Fractal image exercise/sketch.js
+++ Fractal image exercise/sketch.js
@@ -5,24 +5,46 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let rotateAngle;
+const wSnow = 40;
+const hSnow = 40;
+
+let mappedFrameCount;
+let randomAngle;
+let anglesList = [];
+let numSnow = 0;
 
 function setup() {
   createCanvas(500, 500, WEBGL);
   angleMode(DEGREES);
-  rotateAngle = mouseX;
+  for(let i = 0; i < 5; i++){
+    randomAngle = floor(random(0, 360));
+    anglesList.push(randomAngle);
+  }
 }
 
 function draw() {
   background(220);
-  //translate(90, 0, 300);
-  drawBox(50);
+  translate(-130, 0, 100);
+  rotateY(frameCount);
+  //drawSnow(wSnow, hSnow);
+  drawMoreSnow();
 }
 
-function drawBox(w){
-  if(w < 5){
-    rotateZ(rotateAngle);
-    box(w * 0.1);
+function drawSnow(w, h){
+  if(w > 5){
+    for(let i = 0; i < anglesList.length;i++){
+      plane(w,h);
+      rotateZ(i);
+      //rotateX(i);
+      drawSnow(w*0.5, h*0.5);
+    }
+  }
+}
+
+function drawMoreSnow(){
+  if(numSnow < 100){
+    translate(w*0.5, h*0.5, 30);
+    drawSnow(wSnow, hSnow);
   }
 }
 
