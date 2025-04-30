acf72cafcf471a87b5c30bb79198732ac60cc517
GraceL111 <7003958@spsd.sk.ca>
2025-04-30 15:28:58 -0600

    Capstone testing second commit

---
+++ Three.js Test/threejs-test/main.js
@@ -0,0 +1,86 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 100);
+const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls(camera, renderer.domElement);
+renderer.setSize(w,h);
+document.body.appendChild(renderer.domElement);        // add a canvas
+
+scene.background = new THREE.Color(0xff9900);      // background color
+camera.position.z = 5;       // reposition the camera on the z axis
+
+// ----- Add light -----------
+const light = new THREE.DirectionalLight(0x00ff00, 100);
+light.position.set(5, 5, 5);
+scene.add(light);
+
+
+// ------ Create Plane -----------
+const planeGeometry = new THREE.PlaneGeometry(10, 10);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+    side: THREE.DoubleSide });
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = THREE.MathUtils.degToRad(90);
+
+scene.add(plane);
+
+
+
+//----------- Loading glTF file ---------
+
+let model;
+let modelLoaded;
+
+const loader = new GLTFLoader();
+const url = '/assets/Wizard.gltf';
+loader.load(url, (gltf) => {
+    modelLoaded = false;
+    model = gltf.scene;
+    model.scale.set(0.90, 0.90, 0.90);
+    scene.add(model);
+    modelLoaded = true;
+});
+
+// --------animate object -----------
+function animate(){
+    requestAnimationFrame(animate);
+    //model.rotation.x += 0.01;
+    controls.update();
+    if(modelLoaded === true){
+        model.rotation.y += 0.01;
+    }
+    renderer.render(scene, camera);
+}
+
+animate();
+
+
+
+
+
+
+// // ----------Create an object -------------
+// const boxObj = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00} );     // MeshBasicMaterial give only color, they dont care about lights.
+// let cube = new THREE.Mesh(boxObj, material);
+// scene.add(cube);
+
+
+// // ------- outline cube ---------
+// const edges = new THREE.EdgesGeometry(boxObj);          // get edge from cube
+// const lineMaterial = new THREE.LineBasicMaterial({color: 'red'});
+// const line = new THREE.LineSegments(edges, lineMaterial);
+// scene.add(line);
+// cube.add(line);          // keeping line consistant when moving/ rotating
+
+
+
+
+// //renderer.render(scene, camera);        // always put this at the end (rendering after change?)
