4eea1eb2ad34a3383330a42d9cb87ed4ebc66ebd
GraceL111 <7003958@spsd.sk.ca>
2025-05-01 16:04:16 -0600

    Capstone testing third commit

--- Three.js Test/threejs-test/main.js
+++ Three.js Test/threejs-test/main.js
@@ -3,6 +3,9 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 
+
+
+
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
@@ -44,23 +47,66 @@ loader.load(url, (gltf) => {
     modelLoaded = false;
     model = gltf.scene;
     model.scale.set(0.90, 0.90, 0.90);
+    model.rotation.y = THREE.MathUtils.degToRad(180);
     scene.add(model);
     modelLoaded = true;
 });
+
+
+// -----------character movement -----------
+// Create an object to track which keys are pressed
+const keys = {};
+let characterSpeed = 0.05;
+
+// When a key is pressed down
+window.addEventListener('keydown', function(event) {
+  const key = event.key.toLowerCase(); // get the key as lowercase
+  keys[key] = true; // mark that key as "pressed"
+  console.log(keys);
+});
+
+// When a key is released
+window.addEventListener('keyup', function(event) {
+  const key = event.key.toLowerCase(); // get the key as lowercase
+  keys[key] = false; // mark that key as "not pressed"
+});
+
 
 // --------animate object -----------
 function animate(){
     requestAnimationFrame(animate);
     //model.rotation.x += 0.01;
     controls.update();
-    if(modelLoaded === true){
-        model.rotation.y += 0.01;
+    // if(modelLoaded === true){
+    //     model.rotation.y += 0.01;
+    // }
+
+    if(keys['shift'] === true){
+        characterSpeed = 0.10;
     }
+    else{
+        characterSpeed = 0.05;
+    }
+
+    if(keys['w'] === true){
+        model.position.z -= characterSpeed;
+    }
+    if(keys['s'] === true){
+        model.position.z += characterSpeed;
+    }
+    if(keys['a'] === true){
+        model.position.x -= characterSpeed;
+    }
+    if(keys['d'] === true){
+        model.position.x += characterSpeed;
+    }
+
+
+
     renderer.render(scene, camera);
 }
 
 animate();
-
 
 
 
